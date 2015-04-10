8dbca00361cce8b971ac9c44f1c36f66a8c3a065
basbaccarne <bastiaan.baccarne@ugent.be>
2015-04-10 12:44:17 +0200

    Transparent back

--- plot1.R
+++ plot1.R
@@ -1,20 +1,33 @@
-url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-zip <- "exdata%2Fdata%2Fhousehold_power_consumption.zip"
-file <- "household_power_consumption.txt"
+Sys.setlocale("LC_TIME", "English")
+
+loadData <- function(){
+        url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        zip <- "exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        file <- "household_power_consumption.txt"
         
-if (!file.exists(zip)){
-       download.file(url,zip) 
+        if (!file.exists(zip)){
+                cat("downloading file from url ...\n")
+                download.file(url,zip) 
+        }
+        
+        cat("loading dataset ...\n")
+        unzip (zip,files=file)
+        dataset <- read.table(file, header = T, sep = ";", na.strings = "?")
+        
+        cat("subsetting and cleaning dataset ...\n")
+        subset <- subset(dataset, Date == "1/2/2007" | Date == "2/2/2007")
+        subset$Time <- paste(subset$Date,subset$Time, sep = " ") 
+        subset$Time <- strptime(subset$Time, format = "%d/%m/%Y %H:%M:%S")
+        subset$Date <- (as.Date(subset$Date, format = "%d/%m/%Y"))
+        
+        subset
 }
+dataset <- loadData()
 
-dataset <- read.table(unz(zip, file), header = T, sep = ";", na.strings = "?")
-
-subset <- subset(dataset, Date == "1/2/2007" | Date == "2/2/2007")
-subset$Date <- (as.Date(subset$Date, format = "%d/%m/%Y"))
-subset$Time <- strptime(subset$Time, format = "%H:%M:%S")
-
-png(filename="plot1.png", width = 480, height = 480, units = "px")
-hist(subset$Global_active_power, 
+png(filename="plot1.png", width = 480, height = 480, units = "px", bg = "transparent")
+hist(dataset$Global_active_power, 
      col = "red", 
      main = "Global Active Power", 
      xlab = "Global Active Power (kilowatts)")
 dev.off()
+cat("Graph saved in working dir as \"plot1.png\"")
